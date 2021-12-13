46548ccbfd32580f329db98da4d9a58307811894
airatkit <96023983+airatkit@users.noreply.github.com>
2021-12-13 15:54:25 +0300

    Add files via upload

---
+++ app/PointRatingSystem/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+
+namespace PointRatingSystem.Windows
+{
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            var entities = new PointRatingSystemEntities();
+
+            MessageBox.Show(passwordTB.Text);
+
+            foreach (var item in entities.Users)
+            {
+                if (loginTB.Text.Contains(item.Login) && passwordTB.Text.Contains(item.Password))
+                {
+                    Hide();
+
+                    if (item.Role == "Студент")
+                    {
+                        new StudentWindow().Show();
+                    }
+
+                    else if (item.Role == "Преподаватель")
+                    {
+                        new TeacherWindow().Show();
+                    }
+
+                    else if (item.Role == "Администратор")
+                    {
+                        new AdminWindow().Show();
+                    }
+
+                    else if (item.Role == "СотрудникДирекции")
+                    {
+                        new DirectionWindow().Show();
+                    }
+                }
+            }
+
+            MessageBox.Show("O");
+        }
+    }
+}
